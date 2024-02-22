73f9ff4d365ca9711417fe38f1687bb06a82d8fd
edwin141999 <gendevilman@gmail.com>
2024-02-22 16:40:33 -0600

    create constants

---
+++ app/lib/constants.js
@@ -0,0 +1,18 @@
+export const IMAGES_NEWS = [
+  'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_411,c_limit/c10ccfa8-03bb-4641-b52d-a2680fba433d/image.jpg',
+  'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_411,c_limit/fa4ca0d1-080d-494c-a863-620f4c40916f/image.jpg',
+  'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_411,c_limit/0a037571-80fd-4ed6-8130-45cca4e5ff93/image.jpg',
+  'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_411,c_limit/baea93b6-e9ad-49eb-a2c2-b5f10493a1b4/image.png',
+  'https://static.nike.com/a/images/f_auto/dpr_1.0,cs_srgb/w_411,c_limit/0e070445-c712-4d02-8dc3-ce8898bf8baa/image.jpg'
+]
+
+export const IMAGES_MEMBERS = [
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/7cc09330-8e8f-49fa-a5c6-d3a11210bebd/calzado-air-more-uptempo-96-LM2P7n.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_0.8/h_522,c_limit/88dd3883-4b00-462c-9c52-f28cd2033c93/calzado-air-jordan-1-low-se-QqcWXT.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/0062ce46-ff6a-4d16-a697-51f0cf116d4f/playera-de-f%C3%BAtbol-dri-fit-para-antes-del-partido-fc-barcelona-academy-pro-se-QxQkMw.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/4f630e21-6aef-4301-b0f6-4f6fa403a9ed/calzado-air-flight-huarache-lTfTL9.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/77820221-60a5-432b-8239-3359635e78f3/gt-calzado-de-b%C3%A1squetbol-grandes-cut-3-se-PxBFpb.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/f3b6166e-73ef-4502-9edd-addc3175e5ec/chamarra-de-tejido-woven-sportswear-H3J9QN.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/2b8d62aa-cc6d-4397-92c9-1b56c47f6263/playera-de-f%C3%BAtbol-fc-barcelona-vGvKT8.png',
+  'https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_411,c_limit/8c588ec1-5756-4443-84db-89f07d06bae8/jersey-reversible-talla-grande-culture-of-basketball-xzH58Q.png',
+]
