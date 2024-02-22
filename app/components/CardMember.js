5232b723530d6b94191dfe8d5c435df799f69460
edwin141999 <gendevilman@gmail.com>
2024-02-22 17:30:58 -0600

    changes details

--- app/components/CardMember.js
+++ app/components/CardMember.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-export default function CardMember({ img, title, subtitle, price }) {
+export default function CardMember({ img, title, subtitle, price,hidden }) {
   return (
     <article className="flex flex-col min-w-[26rem] gap-2">
       <Image src={img} alt="Sneakers/T-Shirt" width={1000} height={1000} className="w-full" />
       <div className="flex flex-col">
-        <span className="font-semibold text-orange-700">Producto para miembros</span>
+        <span className={`font-semibold text-orange-700 ${hidden}`}>Producto para miembros</span>
         <span className="text-md font-semibold">{title}</span>
         <span className="text-md font-medium text-gray-500">{subtitle}</span>
       </div>
--- app/components/HeaderNews.js
+++ app/components/HeaderNews.js
@@ -4,7 +4,7 @@ export default function HeaderNews({ title }) {
   return (
     <div className="flex flex-row justify-between items-center px-12">
       {/* TODO: Chcear que se pueda scrollear con los botones y poner el disabled al principio y al final*/}
-      <p className="text-2xl">{title}</p>
+      <p className="text-2xl font-medium">{title}</p>
       <div className="flex flex-row gap-3">
         <IconButton src={'/icons/left-arrow.svg'} alt={"Right SVG"} bg={'bg-slate-300 hover:bg-slate-400'} p={'p-3'} />
         <IconButton src={'/icons/right-arrow.svg'} alt={"Right SVG"} bg={'bg-slate-300 hover:bg-slate-400'} p={'p-3'} />
