fca152c7df69f7b71a204e3b6b3e03d04bc88d1e
edwin141999 <gendevilman@gmail.com>
2024-02-22 16:40:57 -0600

    create component header news

---
+++ app/components/HeaderNews.js
@@ -0,0 +1,14 @@
+import IconButton from "./IconButton";
+
+export default function HeaderNews({ title }) {
+  return (
+    <div className="flex flex-row justify-between items-center px-12">
+      {/* TODO: Chcear que se pueda scrollear con los botones y poner el disabled al principio y al final*/}
+      <p className="text-2xl">{title}</p>
+      <div className="flex flex-row gap-3">
+        <IconButton src={'/icons/left-arrow.svg'} alt={"Right SVG"} bg={'bg-slate-300 hover:bg-slate-400'} p={'p-3'} />
+        <IconButton src={'/icons/right-arrow.svg'} alt={"Right SVG"} bg={'bg-slate-300 hover:bg-slate-400'} p={'p-3'} />
+      </div>
+    </div>
+  )
+}
